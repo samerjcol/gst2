643f2a883b9dffd911f109ecbf646a4d3b4e19b0
samerjcol <samer8574jen@gmail.com>
2025-01-26 19:28:37 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: '/gst2/', // Replace 'repository-name' with your GitHub repo name
 });
